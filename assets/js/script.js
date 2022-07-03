8e3b0ec0405fa3fe6923eb38e11c83fa285de193
A <andcooke@duck.com>
2022-07-03 11:04:25 -0500

    added basic bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+const save9El = document.querySelector("#save-button-9")
+const save10El = document.querySelector("#save-button-10")
+const save11El = document.querySelector("#save-button-11")
+const save12El = document.querySelector("#save-button-12")
+const save1El = document.querySelector("#save-button-1")
+const save2El = document.querySelector("#save-button-2")
+const save3El = document.querySelector("#save-button-3")
+const save4El = document.querySelector("#save-button-4")
+const save5El = document.querySelector("#save-button-5")
+
+
+
+
+const taskData = [
+    {
+        time: 9,
+        task: ""
+    },
+    {
+        time: 10,
+        task: ""
+    },
+    {
+        time: 11,
+        task: ""
+    },
+    {
+        time: 12,
+        task: ""
+    },
+    {
+        time: 1,
+        task: ""
+    },
+    {
+        time: 2,
+        task: ""
+    },
+    {
+        time: 3,
+        task: ""
+    },
+    {
+        time: 4,
+        task: ""
+    },
+    {
+        time: 5,
+        task: ""
+    }
+];
+
+
+localStorage.setItem("dummydata", )
+
+
+
+
+localStorage.getItem("dummyData")
